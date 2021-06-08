1aa1c3b5a61c97597ee922f8f188ffb1dfd6e551
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-06-07 22:33:16 -0300

    creation of static UI

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ8G6fjLjvQXBsxnOAK_quF0QDamrvZD4",
+  authDomain: "challenge-5167e.firebaseapp.com",
+  projectId: "challenge-5167e",
+  storageBucket: "challenge-5167e.appspot.com",
+  messagingSenderId: "1000547918557",
+  appId: "1:1000547918557:web:896b2aa7bd9ed56e7db134",
+  measurementId: "G-DQ9XK9S6EP"
+};
